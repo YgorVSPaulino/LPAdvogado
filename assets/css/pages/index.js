b27d796bdf216618109f0d1da50e525b4083bdec
YgorVSPaulino <ygor.paulino.1999@gmail.com>
2024-02-21 15:36:50 -0300

    atualizando mobile

---
+++ assets/css/pages/index.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('nav')
+const botaoMenu = nav.querySelector('#menu')
+const opcoesMenu = nav.querySelector('#opcoes')
+
+
+botaoMenu.addEventListener('click', () => {
+    if (botaoMenu.classList.contains('ativo')) botaoMenu.classList.remove('ativo')
+    else botaoMenu.classList.add('ativo')
+    if (opcoesMenu.classList.contains('mostrar')) opcoesMenu.classList.remove('mostrar')
+    else opcoesMenu.classList.add('mostrar')
+})
+
